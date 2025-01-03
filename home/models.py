f3bd0bd676590347f9e1108658f3cd38cf816482
MattDwyer14 <mattyd799@gmail.com>
2025-01-03 18:51:58 +0000

    added contact form html

--- home/models.py
+++ home/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class contactMessage(models.Model):
+    level = [
+        ('gcse', 'GCSE'),
+        ('alevel', 'A Level')
+    ]
+
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.TextField()
+    subject = models.CharField(max_length=100)
+    level = models.CharField(choices=level )
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name} ({self.email})"
+
 class HomePageContent(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
