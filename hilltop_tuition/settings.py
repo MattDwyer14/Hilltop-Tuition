c3d26238c7e1ed18b5ec8d20726f03b0a610bbee
MattDwyer14 <mattyd799@gmail.com>
2025-04-26 16:12:51 +0100

    making hosts explicit

--- hilltop_tuition/settings.py
+++ hilltop_tuition/settings.py
@@ -14,7 +14,16 @@ if (BASE_DIR / ".env").exists():
 SECRET_KEY    = os.getenv("SECRET_KEY")
 AZURE_DEPLOYED = os.getenv("AZURE_DEPLOYED", os.getenv("azure_deployed", "false")).lower() == "true"
 DEBUG         =  not AZURE_DEPLOYED
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",") if AZURE_DEPLOYED else ["*"]
+if AZURE_DEPLOYED:
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",") if AZURE_DEPLOYED else ["*"]
+    ALLOWED_HOSTS = [
+        "hilltoptuition.com",
+        "www.hilltoptuition.com",
+        # the Azure App Service default host:
+        "mattyswebsite-chcbefabg8c3hsf8.uksouth-01.azurewebsites.net",
+    ]
+else:
+     ALLOWED_HOSTS = ["*"]
 
 # ─── APPS & MIDDLEWARE ─────────────────────────────────────────────────────────
 INSTALLED_APPS = [
