5ea1d070854b7b3282d96651adb17c6f046954be
MattDwyer14 <mattyd799@gmail.com>
2025-04-28 17:36:57 +0100

    redployment of azure resources

--- hilltop_tuition/settings.py
+++ hilltop_tuition/settings.py
@@ -91,6 +91,7 @@ if AZURE_DEPLOYED:
             "NAME":     os.getenv("DB_NAME"),
             "USER":     os.getenv("DB_USER"),
             "HOST":     os.getenv("DB_HOST"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
             "PORT":     os.getenv("DB_PORT", "5432"),
             "OPTIONS":  {"sslmode": "require"},
         }
